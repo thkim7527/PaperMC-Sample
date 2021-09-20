0788e413d0d9a5f94cd113703ea8ee5538de78b6
thkim7527 <thkim7527@gmail.com>
2021-09-20 17:20:07 +0900

    Edit typo & Add @EventHandler

--- src/main/kotlin/me/thkim7527/sample/Event.kt
+++ src/main/kotlin/me/thkim7527/sample/Event.kt
@@ -1,10 +1,12 @@
 package me.thkim7527.sample
 
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 
 class Event: Listener {
+    @EventHandler
     fun onBlockBreakEvent(e: BlockBreakEvent) {
-        e.player.sendMessage("Break Blcok!")
+        e.player.sendMessage("Break Block!")
     }
 }
