c706fe1ffb6503c55bd47d7c3c96df6a2829d004
thkim7527 <thkim7527@gmail.com>
2021-10-10 20:17:30 +0900

    Get WorldEditPlugin from pluginManager

--- src/main/kotlin/me/thkim7527/sample/Plugin.kt
+++ src/main/kotlin/me/thkim7527/sample/Plugin.kt
@@ -1,5 +1,6 @@
 package me.thkim7527.sample
 
+import com.sk89q.worldedit.bukkit.WorldEditPlugin
 import org.bukkit.plugin.java.JavaPlugin
 
 class Plugin: JavaPlugin() {
@@ -7,5 +8,7 @@ class Plugin: JavaPlugin() {
         logger.info("Enabled ${description.fullName}")
         server.pluginManager.registerEvents(Event(), this)
         getCommand("sample")?.setExecutor(Command())
+
+        val WorldEditPlugin: WorldEditPlugin = server.pluginManager.getPlugin("WorldEdit") as WorldEditPlugin
     }
 }
