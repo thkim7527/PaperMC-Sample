1879f9cb5092e8e06a8e93d5e2a65c5c24905a02
thkim7527 <thkim7527@gmail.com>
2022-01-09 19:00:58 +0900

    PaperMC 1.18.1

--- src/main/kotlin/me/thkim7527/sample/Event.kt
+++ src/main/kotlin/me/thkim7527/sample/Event.kt
@@ -1,12 +1,12 @@
 package me.thkim7527.sample
 
+import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.block.BlockBreakEvent
 
 class Event: Listener {
     @EventHandler
-    fun onBlockBreakEvent(e: BlockBreakEvent) {
-        e.player.sendMessage("Break Block!")
+    fun eventHandle(e: Event) {
+
     }
 }
